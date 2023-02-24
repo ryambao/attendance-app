7c0120a54a25a32d2c2b1ccaae93725c5a9f2e1f
Francis John <pelaezfrancisjohn@gmail.com>
2023-02-24 16:43:51 +0800

    created a component for containing DA logo

---
+++ components/_Logo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nuxt-link to="/">
+      <img :src=this.src />
+    </nuxt-link>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      src: "https://www.dreamarchers.com.ph/wp-content/uploads/2020/05/dream-archers-logo.svg"
+    };
+  },
+};
+</script>
+
